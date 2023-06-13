52fcf3b1f361aa74fa025138266a434339faf93b
Ming <chaosflamee@gmail.com>
2023-06-13 10:11:35 +0200

    Update HomeMedecin.vue

--- src/components/MedecinPage/HomeMedecin.vue
+++ src/components/MedecinPage/HomeMedecin.vue
@@ -6,6 +6,14 @@
 export default {
   name: "HomeMedecin"
 }
+
+var http = require('http');
+
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type':'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
+
 </script>
 
 <style scoped>
