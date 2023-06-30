219e2e28b7df039b3b88ed9c606299e4bfc9fd25
Ming <chaosflamee@gmail.com>
2023-06-30 11:35:32 +0200

    Registreation user and pharma

--- src/components/Register/RegisterPharmacie.vue
+++ src/components/Register/RegisterPharmacie.vue
@@ -3,60 +3,61 @@
     <img class="logo_ordotech" src="../../assets/logo/OrdoTech_logo.png">
     <div class="flex">
       <div class="register">
-        <router-link to="/"><p class="info_form">Vous possédez déjà un compte ? Cliquez ici !</p></router-link>
+        <router-link to="/">
+          <p class="info_form">Vous possédez déjà un compte ? Cliquez ici !</p>
+        </router-link>
         <div class="signup_form">
-
           <h1 class="titre_form">Créer un compte</h1>
-          <div class="ligne_container">
-          <div class="ligne">
-            <label><b>Nom de la pharmacie</b></label>
-            <input type="text" v-model="nom" placeholder="Nom" name="nom" required>
-          </div>
+          <form @submit.prevent="registerToTable">
+            <div class="ligne_container">
+              <div class="ligne">
+                <label><b>Nom de la pharmacie</b></label>
+                <input type="text" v-model="nom" placeholder="Nom" name="nom" required>
+              </div>
 
-          <div class="ligne">
-            <label><b>E-mail</b></label>
-            <input type="text" v-model="email" placeholder="E-mail" name="prenom" required>
-          </div>
-          </div>
+              <div class="ligne">
+                <label><b>E-mail</b></label>
+                <input type="text" v-model="email" placeholder="E-mail" name="prenom" required>
+              </div>
+            </div>
 
-
-
-          <div class="ligne">
-            <label><b>Numéro de téléphone</b></label>
-            <input type="number" v-model="numero" placeholder="Numéro" name="mail" required>
-          </div>
-
-          <div class="ligne_container">
-          <div class="ligne">
-            <label><b>N°</b></label>
-            <input type="text" v-model="adresse" placeholder="N°" name="adresse" required>
-          </div>
-          <div class="ligne">
-            <label><b>Rue</b></label>
-            <input type="text" v-model="adresse" placeholder="Rue" name="adresse" required>
-          </div>
-          </div>
-
-          <div class="ligne_container">
             <div class="ligne">
-              <label><b>Code Postal</b></label>
-              <input type="number" v-model="numero" placeholder="COde Postal" name="numero" required>
+              <label><b>Numéro de téléphone</b></label>
+              <input type="number" v-model="numero" placeholder="Numéro" name="mail" required>
+            </div>
+
+            <div class="ligne_container">
+              <div class="ligne">
+                <label><b>N°</b></label>
+                <input type="text" v-model="nb_adresse" placeholder="N°" name="adresse" required>
+              </div>
+              <div class="ligne">
+                <label><b>Rue</b></label>
+                <input type="text" v-model="adresse" placeholder="Rue" name="adresse" required>
+              </div>
+            </div>
+
+            <div class="ligne_container">
+              <div class="ligne">
+                <label><b>Code Postal</b></label>
+                <input type="number" v-model="code_postale" placeholder="Code Postal" name="numero" required>
+              </div>
+
+
+              <div class="ligne">
+                <label><b>Ville</b></label>
+                <input type="text" v-model="city" placeholder="Ville" name="adresse" required>
+              </div>
+            </div>
+
+            <div class="ligne">
+              <label><b>Mot de Passe</b></label>
+              <input type="password" v-model="mdp" placeholder="Mot de Passe" name="mdp" required>
             </div>
 
 
-          <div class="ligne">
-            <label><b>Ville</b></label>
-            <input type="text" v-model="adresse" placeholder="Ville" name="adresse" required>
-          </div>
-          </div>
-
-          <div class="ligne">
-            <label><b>Mot de Passe</b></label>
-            <input type="password" v-model="mdp" placeholder="Mot de Passe" name="mdp" required>
-          </div>
-
-
-          <button class="signupButton">M'inscrire !</button>
+            <button class="signupButton">M'inscrire !</button>
+          </form>
         </div>
       </div>
     </div>
@@ -64,11 +65,47 @@
 </template>
 
 <script>
+import axios from "axios";
+import { createTextVNode } from "vue";
 export default {
-  name: "RegisterPharmacie"
+  name: "RegisterPharmacie",
+
+  data() {
+    return {
+      nom: '',
+      email: '',
+      numero: '',
+      nb_adresse: '',
+      adresse: '',
+      code_postale: '',
+      city: '',
+      mdp: ''
+    }
+  },
+  methods: {
+    registerToTable(){
+      let data = {
+        nom: this.nom,
+        email: this.email,
+        numero : this.numero,
+        nb_adresse: this.nb_adresse,
+        adresse: this.adresse,
+        code_postale: this.code_postale,
+        city: this.city,
+        mdp: this.mdp
+      };
+
+      axios.post("http://localhost:5001/inscriptionPharmacie", data)
+      .then(response => {
+          // console.log(response.data)
+          console.log("Pharmacie registeration success!")
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  }
 }
 </script>
 
-<style src="../../style/login-register.css">
-
-</style>
+<style src="../../style/login-register.css"></style>
--- src/components/Register/RegisterUser.vue
+++ src/components/Register/RegisterUser.vue
@@ -3,52 +3,59 @@
     <img class="logo_ordotech" src="../../assets/logo/OrdoTech_logo.png">
     <div class="flex">
       <div class="register">
-        <router-link to="/"><p class="info_form">Vous possédez déjà un compte ? Cliquez ici !</p></router-link>
+        <router-link to="/">
+          <p class="info_form">Vous possédez déjà un compte ? Cliquez ici !</p>
+        </router-link>
         <div class="signup_form">
-
           <h1 class="titre_form">Créer un compte</h1>
+          <form @submit.prevent="registerToTable">
+            <div class="ligne_container">
+              <div class="ligne">
+                <label><b>Nom</b></label>
+                <input type="text" v-model="nom" placeholder="Nom" name="nom" required>
+              </div>
 
-          <div class="ligne_container">
-          <div class="ligne">
-            <label><b>Nom</b></label>
-            <input type="text" v-model="nom" placeholder="Nom" name="nom" required>
-          </div>
+              <div class="ligne">
+                <label><b>Prénom</b></label>
+                <input type="text" v-model="prenom" placeholder="Prénom" name="prenom" required>
+              </div>
+            </div>
 
-          <div class="ligne">
-            <label><b>Prénom</b></label>
-            <input type="text" v-model="prenom" placeholder="Prénom" name="prenom" required>
-          </div>
-          </div>
+            <div class="ligne_container">
+              <div class="ligne">
+                <label><b>E-mail</b></label>
+                <input type="email" v-model="mail" placeholder="E-mail" name="mail" required>
+              </div>
 
-          <div class="ligne_container">
-          <div class="ligne">
-            <label><b>E-mail</b></label>
-            <input type="email" v-model="mail" placeholder="E-mail" name="mail" required>
-          </div>
+              <div class="ligne">
+                <label><b>Numéro de téléphone</b></label>
+                <input type="number" v-model="numero" placeholder="Numéro" name="numero" required>
+              </div>
+            </div>
+            <div class="ligne">
+              <label><b>Numéro de sécurité sociale</b></label>
+              <input type="number" v-model="secu" placeholder="Sécurité Sociale" name="adresse" required>
+            </div>
 
-          <div class="ligne">
-            <label><b>Numéro de téléphone</b></label>
-            <input type="number" v-model="numero" placeholder="Numéro" name="numero" required>
-          </div>
-          </div>
-          <div class="ligne">
-            <label><b>Numéro de sécurité sociale</b></label>
-            <input type="number" v-model="secu" placeholder="Sécurité Sociale" name="adresse" required>
-          </div>
+            <div class="ligne">
+              <label><b>Mot de Passe</b></label>
+              <input type="password" v-model="mdp" placeholder="Mot de Passe" name="mdp" required>
+            </div>
 
 
-          <div class="ligne">
-            <label>Médecin traitant</label>
-            <select name="Medecins" id="medecin-select">
-            </select>
-          </div>
+            <div class="ligne">
+              <label>Médecin traitant</label>
+              <select v-model="selectedMedecin" id="medecin-select">
+                <option v-for="medecin in medecins" :value="medecin.id_medecin" :key="medecin.id_medecin">
+                  {{ medecin.first_name + ' ' + medecin.last_name }}
+                </option>
+              </select>
+              <!-- <select name="Medecins" id="medecin-select">
+              </select> -->
+            </div>
 
-          <div class="ligne">
-            <label><b>Mot de Passe</b></label>
-            <input type="password" v-model="mdp" placeholder="Mot de Passe" name="mdp" required>
-          </div>
-
-          <button class="signupButton">M'inscrire !</button>
+            <button class="signupButton" type="submit">M'inscrire !</button>
+          </form>
         </div>
       </div>
     </div>
@@ -57,37 +64,32 @@
 
 <script>
 import axios from 'axios'
+
 export default {
   name: "RegisterUser",
-  data(){
-    return{
-      secu : null,
-      mail : '',
-      nom : '',
-      prenom : '',
-      mdp : '',
-      numero : null,
-      medecins : []
+  data() {
+    return {
+      secu: null,
+      mail: '',
+      nom: '',
+      prenom: '',
+      mdp: '',
+      numero: null,
+      medecins: [],
+      selectedMedecin: null,
     }
   },
-  created(){
+  created() {
     this.getMedecins()
   },
-  methods : {
-    getMedecins(){
-      axios.get("http://localhost:5001/getMedecin").then(response =>{
-        console.log(response.data)
-        this.medecins = response.data
-        this.fillSelectMedecins()
-      })
-    },
+  methods: {
     modifyList(objets) {
       var resultat = [];
 
       for (var i = 0; i < objets.length; i++) {
         var objet = objets[i];
         var nouvelObjet = {
-          text: objet.first_name+" "+objet.last_name
+          text: objet.first_name + " " + objet.last_name
         };
 
         if (i === 0) {
@@ -106,17 +108,41 @@ export default {
       let listOptions = this.modifyList(this.medecins)
 
       for (var i = 0; i < listOptions.length; i++) {
-        var drug = listOptions[i];
-        selectBox.add(new Option(drug.text, drug.text, drug.selected));
+        var medecin = listOptions[i];
+        selectBox.add(new Option(medecin.text, medecin.text, medecin.selected));
       }
     },
+    getMedecins() {
+      axios.get("http://localhost:5001/getMedecinIdName")
+        .then(response => {
+          // console.log("Medecin list"+response.data)
+          this.medecins = response.data
+          // this.fillSelectMedecins()
+        })
+    },
+    registerToTable() {
+      let user = {
+        secu: this.secu,
+        mail: this.mail,
+        nom: this.nom,
+        prenom: this.prenom,
+        mdp: this.mdp,
+        numero: this.numero,
+        selectedMedecin: this.selectedMedecin
+      };
+      //console.log(user.selectedMedecin)
+      axios.post("http://localhost:5001/inscriptionStudent", user)
+        .then(response => {
+          // console.log(response.data)
+          console.log("Patient registeration success!")
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+
+    }
   }
 }
 </script>
 
-<style src="../../style/login-register.css">
-
-
-
-
-</style>
+<style src="../../style/login-register.css"></style>
