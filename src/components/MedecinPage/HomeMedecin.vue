b4f6ce2fb392f6f2fb3b1b40f2027c939dea241a
Amine2111 <94388887+Amine2111@users.noreply.github.com>
2023-07-01 23:20:45 +0200

    Ajout de la fonction sendMail

--- src/components/MedecinPage/HomeMedecin.vue
+++ src/components/MedecinPage/HomeMedecin.vue
@@ -104,6 +104,28 @@ export default {
     this.getEtudiantsSuivi()
   },
   methods : {
+    async sendEmail(ordo) {
+        // préparer les informations de l'email
+        const mailData = {
+            to: 'destinationEmail@gmail.com', // mettez l'email de la destination ici
+            subject: `Ordonnance pour ${ordo.first_name} ${ordo.last_name}`,
+            text: `Ordonnance pour ${ordo.first_name} ${ordo.last_name} a été générée le ${this.formatDate(ordo.date)}`
+        };
+        
+        try {
+            // envoyer une requête POST à l'API de votre serveur pour envoyer l'email
+            const response = await axios.post('http://localhost:5001/sendmail', mailData);
+
+            // afficher le message de succès ou d'échec selon la réponse du serveur
+            if (response.data.status === 'success') {
+                alert('Email sent successfully');
+            } else {
+                alert('Failed to send email: ' + response.data.message);
+            }
+        } catch (error) {
+            console.error('Failed to send email: ', error);
+        }
+    },
 
     getOrdonnances(){
       axios.post("http://localhost:5001/getOrdonnances", {
