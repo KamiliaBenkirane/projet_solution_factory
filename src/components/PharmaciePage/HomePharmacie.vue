db34b56008fd4633ee143060d514210fa416e1d0
Ming <chaosflamee@gmail.com>
2023-07-03 11:14:33 +0200

    Update HomePharmacie.vue

--- src/components/PharmaciePage/HomePharmacie.vue
+++ src/components/PharmaciePage/HomePharmacie.vue
@@ -7,6 +7,32 @@
       <div><input type="search" id="recherche_ordo" name="recherche_ordo" placeholder="Entrez un N° Sécurité Sociale" v-model="searchQuery">
         <button><img class="icone_chercher" src="../../assets/icones/chercher.png" alt="logo_chercher"></button>
       </div>
+
+      <!-- <div class="liste_ordo">
+          <div v-for="ordo in ordonnances" :key="ordo.id" class="ordo_item">
+            <div class="titre_ordo">
+              <img class="icone_ordo" src="../../assets/icones/ordo_icone.png" alt="icone_ordo">
+              <h3>ID Ordo: {{ ordo.id_ordo }}</h3>
+            </div>
+            <div class="bloc_info_ordo">
+              <div>
+                <p><i class='bx bxs-envelope'></i> E-mail: {{ ordo.email }}</p>
+                <p><i class='bx bxs-phone'></i> Numéro : {{ ordo.num_phone }}</p>
+
+                <p v-if="ordo.address">
+                  <i class='bx bxs-map'></i> Address: {{ ordo.address.nb_street }} {{ ordo.address.street_name }}, {{
+                    ordo.address.post_code }}{{ ordo.address.city }}
+                </p>
+              </div>
+              <div>
+                <p class="bouton_acces" @click="sendToPharma(ordo.id_ordo)">Autoriser l'accès à l'ordonnance <i
+                    class='bx bxs-lock-open'></i></p>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
+
     </div>
   </div>
 
@@ -14,6 +40,9 @@
 
 <script>
 import SidebarPharmacie from "@/components/PharmaciePage/SidebarPharmacie.vue";
+import { response } from "express";
+import { useSessionStore } from "@/stores/session";
+import axios from 'axios';
 
 export default {
   name: "HomePharmacie",
@@ -23,8 +52,33 @@ export default {
   data(){
     return{
       searchQuery:'',
+      ordonnances: [],
     }
   },
+
+  created(){
+    //this.getOrdonnances()
+  },
+
+  methods:{
+    //the table of ordonnance_pharma
+    // getOrdonnances(){
+    //   axios.post('http://localhost:5001/getOrdonnancePharma',{
+    //     role: "id_pharma",
+    //     id: this.store.getId()
+    //   }).then(response => {
+    //     this.ordonnances = response.data;
+    //   }).catch(err => {
+    //     console.log(err)
+    //   })
+    // },
+
+    // getDetailedOrdonnances(){
+    //   axios.post
+    // }
+
+
+  }
 
 }
 </script>
